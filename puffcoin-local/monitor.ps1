e71a4259eb32693b454c55664929af0e1236fe99
dvlop <denis77@gmail.com>
2025-05-07 18:43:09 +0300

    local and monitoring

--- puffcoin-local/monitor.ps1
+++ puffcoin-local/monitor.ps1
@@ -1,11 +1,18 @@
 while ($true) {
     $time = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
     Write-Host "Time: $time"
-    
-    docker exec puffcoin-node1 ./puffcoind getblockcount
-    docker exec puffcoin-node1 ./puffcoind getbalance
-    docker exec puffcoin-node2 ./puffcoind getbalance
 
+    $blockcount = docker exec puffcoin-node1 ./puffcoind getblockcount
+    $balance1 = docker exec puffcoin-node1 ./puffcoind getbalance
+    $balance2 = docker exec puffcoin-node2 ./puffcoind getbalance
+    $peers = docker exec puffcoin-node1 ./puffcoind getpeerinfo | ConvertFrom-Json
+    $peerCount = $peers.Count
+
+    Write-Host "Block Height: $blockcount"
+    Write-Host "Balance Node 1: $balance1"
+    Write-Host "Balance Node 2: $balance2"
+    Write-Host "Connected Peers: $peerCount"
     Write-Host "------------------------------`n"
+
     Start-Sleep -Seconds 10
 }
--- puffcoin-local/monitor.sh
+++ puffcoin-local/monitor.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 while true; do
   echo "Time: $(date)"
+  echo -n "Block Height: "
   docker exec puffcoin-node1 ./puffcoind getblockcount
+  echo -n "Balance Node 1: "
   docker exec puffcoin-node1 ./puffcoind getbalance
+  echo -n "Balance Node 2: "
   docker exec puffcoin-node2 ./puffcoind getbalance
-  echo "-----------"
+  echo -n "Connected Peers: "
+  docker exec puffcoin-node1 ./puffcoind getpeerinfo | jq length
+  echo "------------------------------"
   sleep 10
 done
